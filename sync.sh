f4b0539a2c588c4179b510b82d415d7e90838cce
Matthias Pfefferle <pfefferle@gmail.com>
2017-05-21 21:36:49 +0200

    1.2.0

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-httrack http://spdx.org/licenses/ \
+httrack https://spdx.org/licenses/ \
   -O SPDX_Licenses.docset/Contents/Resources/Documents,cache -I0 \
   --display=2 --timeout=60 --retries=99 --sockets=7 \
   --connection-per-second=5 --max-rate=250000 \
   --keep-alive --depth=4 --mirror --robots=0 \
   --user-agent '$(httrack --version); dash-spdx-licenses ()' \
-  "-*" "+http://spdx.org/licenses/*" "-http://spdx.org/licenses/[*"
+  "-*" "+https://spdx.org/licenses/*" "-https://spdx.org/licenses/[*"
